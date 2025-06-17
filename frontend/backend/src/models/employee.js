3f193a3f5fcef8698668ecb4d35344f61cf25977
trizo1104 <vominhtri221104@gmail.com>
2025-06-17 12:45:09 +0700

    Initial commit

--- frontend/backend/src/models/employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String, required: true },
-  assignedBookings: [{ type: mongoose.Schema.Types.ObjectId, ref: "Booking" }],
-});
-
-module.exports = mongoose.model("Employee", employeeSchema);
--- frontend/backend/src/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authController = require("../controllers/auth.controller");
-const authRoute = express.Router();
-
-authRoute.post("/signup", authController.signUp);
-authRoute.post("/signin", authController.signIn);
-authRoute.post("/signout", authController.signOut);
-
-module.exports = authRoute;
