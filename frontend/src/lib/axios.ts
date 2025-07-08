7e017ba5fb1c333390fc60649805f7631bcb290b
trizo1104 <vominhtri221104@gmail.com>
2025-07-08 14:33:29 +0700

    update

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://cleanning-booking.onrender.com/api",
   // baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
