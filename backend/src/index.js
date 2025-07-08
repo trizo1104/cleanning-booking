9cf3b6dac6732555e58d8093ff6eabd029287713
trizo1104 <vominhtri221104@gmail.com>
2025-07-08 16:21:45 +0700

    fix bug BE

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://cleanning-booking.vercel.app/"],
+    origin: ["http://localhost:3000", "https://cleanning-booking.vercel.app"],
     credentials: true,
   })
 );
