b234b5d219bf23471194f0fe385cb185e8cd2738
trizo1104 <vominhtri221104@gmail.com>
2025-07-08 14:40:58 +0700

    update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ const generateToken = (userId, role, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
-    secure: false,
+    secure: true,
   });
 
   return token;
