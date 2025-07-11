f99cf0ae15a4ea484e9d315f185860e67ab9d469
trizo1104 <vominhtri221104@gmail.com>
2025-07-11 13:48:39 +0700

    fix payment

--- frontend/src/app/(payment)/checking-payment/page.tsx
+++ frontend/src/app/(payment)/checking-payment/page.tsx
@@ -19,7 +19,7 @@ const CheckingPaymentInner = () => {
       if (!transactionId) return router.push("/payment-failure");
       try {
         const res = await axios.get(
-          `http://localhost:8080/api/payment-zalo/check-payment-status/${transactionId}`
+          `https://cleanning-booking.onrender.com/api/payment-zalo/check-payment-status/${transactionId}`
         );
         if (res.data.success && res.data.status === "pending") {
           setStatus("redirecting");
@@ -44,25 +44,25 @@ const CheckingPaymentInner = () => {
   }, [router, transactionId]);
 
   return (
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#e0f7fa] to-[#fce4ec]">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.6 }}
-          className="bg-white shadow-xl rounded-2xl px-8 py-12 text-center max-w-md w-full border border-gray-100"
-        >
-          <div className="flex flex-col items-center space-y-6">
-            <Loader2 className="w-16 h-16 text-blue-500 animate-spin" />
-            <h1 className="text-2xl font-bold text-gray-800">
-              Đang kiểm tra trạng thái thanh toán ZaloPay...
-            </h1>
-            <p className="text-gray-500 text-sm max-w-sm">
-              Vui lòng không đóng trình duyệt trong khi chúng tôi xác nhận giao
-              dịch của bạn.
-            </p>
-          </div>
-        </motion.div>
-      </div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#e0f7fa] to-[#fce4ec]">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6 }}
+        className="bg-white shadow-xl rounded-2xl px-8 py-12 text-center max-w-md w-full border border-gray-100"
+      >
+        <div className="flex flex-col items-center space-y-6">
+          <Loader2 className="w-16 h-16 text-blue-500 animate-spin" />
+          <h1 className="text-2xl font-bold text-gray-800">
+            Đang kiểm tra trạng thái thanh toán ZaloPay...
+          </h1>
+          <p className="text-gray-500 text-sm max-w-sm">
+            Vui lòng không đóng trình duyệt trong khi chúng tôi xác nhận giao
+            dịch của bạn.
+          </p>
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
