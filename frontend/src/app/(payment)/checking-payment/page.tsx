dc761dac05e56618f1c1e84c967f99d667094472
trizo1104 <vominhtri221104@gmail.com>
2025-07-11 14:03:49 +0700

    update

--- frontend/src/app/(payment)/checking-payment/page.tsx
+++ frontend/src/app/(payment)/checking-payment/page.tsx
@@ -1,6 +1,15 @@
 "use client";
+import { Suspense } from "react";
 import CheckingPaymentInner from "./CheckingPaymentInner";
 
 export default function Page() {
-  return <CheckingPaymentInner />;
+  return (
+    <Suspense
+      fallback={
+        <div className="text-center py-20">Đang tải trang thanh toán...</div>
+      }
+    >
+      <CheckingPaymentInner />;
+    </Suspense>
+  );
 }
