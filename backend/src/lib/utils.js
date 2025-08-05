d19ad110f0ad7a4882a55523763afb4e288b55ef
trizo1104 <vominhtri221104@gmail.com>
2025-08-05 09:45:11 +0700

    check

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ const generateToken = (userId, role, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "Strict",
     secure: true,
   });
 
