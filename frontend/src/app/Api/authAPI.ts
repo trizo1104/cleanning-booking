19d90d2392d67f2de8ab5d83e36e401e701ba63f
trizo1104 <vominhtri221104@gmail.com>
2025-08-05 08:44:10 +0700

    fix

--- frontend/src/app/Api/authAPI.ts
+++ frontend/src/app/Api/authAPI.ts
@@ -3,7 +3,7 @@ export const loginEmployeeAPI = "/auth/signin-employee";
 export const signupEmployeeAPI = "/auth/signup-employee";
 export const signUpAPI = "/auth/signup";
 export const logoutAPI = "/auth/signOut";
-export const meAPI = "auth/me";
+export const meAPI = "/auth/me";
 export const getAllUserAPI = "auth/getalluser";
 export const getAllEmployeeAPI = "auth/getallemployee";
 export const deleteUserAPI = (id: string) => {
